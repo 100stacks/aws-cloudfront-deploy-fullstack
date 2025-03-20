558f917b7383e1bda6aa6f5f99649f37f7a6fe64
100stacks <100stacks@users.noreply.github.com>
2025-03-19 23:24:22 -0400

    aws(s3): deploy nextjs app to S3 Bucket

--- backend/src/stacks/BedrockAIStack.ts
+++ backend/src/stacks/BedrockAIStack.ts
@@ -1,15 +1,60 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 
 export class BedrockAIStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // s3 bucket where construct will reside
+    const bucket = new s3.Bucket(this, "WebsiteS3Bucket", {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "404.html",
+      publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CloudFront construct
+    const distro = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "WebsiteCloudFrontDist",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: bucket,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    // s3 construct to deploy the website dist content
+    new s3deploy.BucketDeployment(this, "WebsiteS3BucketDeploy", {
+      destinationBucket: bucket,
+      sources: [s3deploy.Source.asset("../apps/frontend/dist")],
+      distribution: distro,
+      distributionPaths: ["/*"],
+    });
+
+    // CloudFormation Output construct (i.e., console log for CloudFormation CDK)
+    new cdk.CfnOutput(this, "webUrl", {
+      exportName: "webUrl",
+      value: `https://${distro.distributionDomainName}`,
+    });
   }
 }
