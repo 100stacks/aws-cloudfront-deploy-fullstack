c7efc8148a35a337b5e1cf3f0e6a8e7ac89169de
100stacks <100stacks@users.noreply.github.com>
2025-03-19 22:29:55 -0400

    wip: refactor backend | AWS deploy preps

--- backend/src/stacks/BedrockAIStack.ts
+++ backend/src/stacks/BedrockAIStack.ts
@@ -1,8 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
-export class BackendStack extends cdk.Stack {
+export class BedrockAIStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
